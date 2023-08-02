ddc51dbcc0d446eb4e5c2f0c842e6c7a595edc00
Ezequiel Benitez <ezequielbv@gmail.com>
2023-08-02 09:12:23 +0200

    Added score descriptions and dice icons

--- src/Die.js
+++ src/Die.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps = {
+    numWords: ["one", "two", "three", "four", "five", "six"],
+  };
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
@@ -10,15 +13,11 @@ class Die extends Component {
     this.props.handleClick(this.props.idx);
   }
   render() {
-    return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.handleClick}
-      >
-        {this.props.val}
-      </button>
-    );
+    const { numWords, locked, val, disabled } = this.props;
+    let classes = `Die fas fa-dice-${numWords[val - 1]} fa-5x `;
+    if (locked) classes += "Die-locked";
+
+    return <i className={classes} onClick={this.handleClick} disabled={disabled}/>;
   }
 }
 
